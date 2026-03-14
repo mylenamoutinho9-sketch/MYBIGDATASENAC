95a8b3f67314268c6a2062af895992e1d0d8d745
mylenamoutinho9-sketch <mylenamoutinho9@gmail.com>
2026-03-13 21:44:51 -0300

    atualiza a pasta git

---
+++ AULA13032026.py
@@ -0,0 +1,73 @@
+# #Laços de repetição
+
+# intervalo1 = range(10)
+# print(intervalo1)
+
+# print('***')
+
+# for numero in range(1,10,2):
+#     print('CONTANDO:')
+#     print('número:')
+# #     print(numero)
+
+# for i in range(5):
+#     try:
+#         #i representa o numero atual da repetição
+#         print(f"Número {i + 1} de {5}:")
+#         num = float(input("Digite um número"))
+
+#         dobro = num * 2
+#         triplo = num * 3
+#         quadruplo = num * 4
+
+#         print(f" Resultado: Dobro={dobro}, Triplo={triplo}, Quádruplo={quadruplo}\n")
+
+#     except ValueError:
+#         print("Entrada inválida. Tente novamente")
+
+#>>> WHILE
+
+# # loolap=float(input('Qual é o valor do evento(inteira)?'))
+# loolap=301
+
+# while loolap > 300:
+#     loolap=float(input('Qual é o valor do evento(inteira)?'))
+#     print('Mas não vou mesmo!!!')
+
+# print('Estarei lá!')
+
+#DONTPAD
+
+# loolap=float(input('Qual o valor do evento (inteira)?')) #Dependnedo do input, cai num loop infinito
+
+# while loolap > 300:
+#     print('Mas não vou mesmo!!!')
+
+# print('Estarei lá!')
+# ##
+
+# loolap=0
+
+# while loolap > 300:
+#     loolap=float(input('Qual o valor do evento (inteira)?')) #Dependnedo do input, cai num loop infinito
+#     print('Mas não vou mesmo!!!')
+
+# print('Estarei lá!')
+# ##
+
+# loolap=300
+
+# while loolap >= 300:
+#     loolap=float(input('Qual o valor do evento (inteira)?')) #Dependnedo do input, cai num loop infinito
+#     print('Mas não vou mesmo!!!')
+
+# print('Estarei lá!')
+
+cadidato=float(input('Qual a idade do candidato(inteira)?'))
+
+while cadidato < 18:
+    print('Idade invalida!')
+
+while candidato2 >= 18:
+    print('informe CPF e email do candidato')
+
---
+++ aula14032026/aula1403.py
@@ -0,0 +1,5 @@
+#LISTAS:
+#índices:   0        1       2        3       4
+nomes = ['Matheus','Alice','Caio','Larissa','Miguel']
+
+print(nomes[0])
