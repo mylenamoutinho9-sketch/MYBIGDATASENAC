a93ace5e6fd55acccefac9de7d6a1229b681b98c
mylenamoutinho9-sketch <mylenamoutinho9@gmail.com>
2026-03-16 21:47:51 -0300

    atualiza pasta git

---
+++ aula.16032026.py
@@ -0,0 +1,38 @@
+#Média do Aluno com Optativa
+
+# nota1=abs(float(input('Primeira Nota:')))
+# nota2=abs(float(input('Segunda Nota:')))
+# optativa=float(input('Tem nota optativa?'))
+
+
+# if optativa == -1:
+#     media=(nota1+nota2)/2
+# else:
+#     if optativa > nota1:
+#         media=(optativa+nota2)/2
+#     else:
+#         media=(optativa+nota1)/2
+
+# if media >= 6.0:
+#     resultado='APROVADO'
+# elif media >= 3.0:
+#     resultado='RECUPERAÇÃO'
+# else:
+#     resultado='REPROVADO'
+
+# print(f'Média final: {media}; resultado: {resultado}')
+
+#Cálculo de Lâmpadas
+
+# lampadas = 0
+# POTENCIA = 3
+# largura = float(input('Largura:'))
+# comprimento=float(input('Comprimento:'))
+
+# area=largura*comprimento
+
+# lampadas=area/POTENCIA
+
+# print(f'são necessárias {round(lampadas)} lâmpadas para para iluminar um cômodo {area} m2.')
+
+# lampadas.round(2)
