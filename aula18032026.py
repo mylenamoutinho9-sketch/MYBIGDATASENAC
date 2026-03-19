0ed29e1318212fb676245e97ce21fb14cf09f99c
mylenamoutinho9-sketch <mylenamoutinho9@gmail.com>
2026-03-18 21:47:29 -0300

    atualizar git

---
+++ aula18032026.py
@@ -0,0 +1,49 @@
+
+# # nomes_att=['Daniela Correia','Luciene Silva','Rafael Fernandes']
+# # sobrenomes=[8]
+# # dani=nomes_att[0][8:]
+# # print(dani)
+
+# # nomes_att=['Daniela Correia','Luciene Silva','Rafael Fernandes']
+# # dani=nomes_att[0][8:]
+# # luciene=nomes_att[1][8:]
+# # rafael=nomes_att[2][7:]
+# # sobrenomes=[dani,luciene,rafael]
+# # print(sobrenomes)
+
+# nomes_att=['Daniela Correia','Luciene Silva','Rafael Fernandes']
+# dani=nomes_att[0][8:]
+# luciene=nomes_att[1][8:]
+# rafael=nomes_att[2][7:]
+# sobrenomes=[]
+# # for i in nomes_att:
+# #     for i in nomes_att[0]:
+# #         sobrenomes.append(nomes_att)
+# # print(sobrenomes)
+
+# nome5='luciene'
+# nome4='Miguel'
+# nomes_att.insert(2,nome4) #adiciona em uma posição exata
+# nomes_att.append(nome5) #
+# nomes_att.append(nome5)
+# # nomes_att.remove('Luciene')
+# # print(nomes_att.index('Luciene'))
+# nomes_att.clear()
+# print(nomes_att)
+
+Impares={33,5,11,27,11,71,79,99,15}
+print(Impares)
+print(type(Impares))
+
+
+filme={
+    'nome':'V for Vendetta',
+    'ano':'2005',
+    'Genero':'ação',
+    'faixa_etaria':16
+}
+
+print(filme)
+print(type(filme))
+
+filme['duracao']='130mi'
---
+++ aula18032026/aula1803.py
@@ -0,0 +1,45 @@
+#LISTAS:
+#índices:   0        1       2        3       4
+nomes = ['Matheus','Alice','Caio','Larissa','Miguel']
+print(nomes)
+
+# print(nomes[0])
+
+# #DONTPAD
+
+# nomes = ['Matheus','Alice','Caio','Larissa','Miguel','Rafael']
+
+# nome1 = nomes[0]
+
+# print(nomes[-1])
+# print(nomes[-2])
+# print(nomes[-3])
+# print(nomes[-4])
+# print(nome1)
+
+# primeira_parte=nomes[0:3]
+# print(primeira_parte)
+# segunda_parte=nomes[3:6]
+# print(segunda_parte)
+
+# print(len(nomes)) #saber a quantidade de elementos das listas
+
+#lista1 = [] - mutavel
+ #nomes = ['Matheus','Alice','Caio','Larissa','Miguel','Rafael']
+
+#tupla1 = () - fixo
+nomes_tupla=tuple(nomes)
+print(nomes_tupla)
+
+#set1 = () ou {} - mutavel e desordenado
+nomes_set=set(nomes)
+print(nomes_set)
+
+#dict1 = {:} - elementos e seus significados
+nomes_dict={'nome1':'Matheus',
+            'nome2':'Alice',
+            'nome3':'Caio',
+            'nome4':'Larissa',
+            'nome5':'Miguel',
+            'nome6':'Rafael'} #podem ser associados apenas a números
+print(nomes_dict)
